babbbb3ee19fdb8ce17c5fde9b6bc367acaf5b2b
nonthong <narathip6007@gmail.com>
2018-02-25 17:36:04 +0700

    adminController

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("loing.fxml"));
         primaryStage.setTitle("School Management System");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 600, 390));
         primaryStage.show();
     }
 
